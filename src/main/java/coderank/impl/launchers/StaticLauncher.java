dfd826ebbdc74553190150647733b72009fca73f
Sergey Reznick <sergey.reznick@gmail.com>
2021-12-06 02:36:56 +0300

    Added new launcher

--- src/main/java/coderank/impl/launchers/StaticLauncher.java
+++ src/main/java/coderank/impl/launchers/StaticLauncher.java
@@ -5,31 +5,35 @@ import coderank.impl.staticanalysis.Configuration;
 import coderank.impl.javagraph.Graph;
 import coderank.impl.javagraph.MethodNode;
 import coderank.impl.graphbuilder.GraphBuilderLoader;
+import one.util.streamex.StreamEx;
 import org.objectweb.asm.ClassReader;
 
 import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.stream.Collectors;
 
 public class StaticLauncher {
 
     public static GraphBuilderLoader<MethodNode> loader;
 
-    public static void launchStatic(String[] args, Enumeration<JarEntry> entries, JarFile jarFile) throws Exception {
-
-        loader = new GraphBuilderLoader<>(args[1], args[2]);
+    public static void launchStatic(String graphBuilderLocation, String graphBuilderClass, JarFile jarFile) throws Exception {
+        loader = new GraphBuilderLoader<>(graphBuilderLocation, graphBuilderClass);
         loader.createInstance();
-        while (entries.hasMoreElements()) {
-            JarEntry entry = entries.nextElement();
+        List<JarEntry> entries =  jarFile.stream().filter(entry -> {
             String name = entry.getName();
-            if (name.endsWith(".class") && Configuration.processPackage(name)) {
-                try (InputStream stream = new BufferedInputStream(jarFile.getInputStream(entry), 1024)) {
-                    ClassReader re = new ClassReader(stream);
-                    ClassDescriptor cv = new ClassDescriptor(stream);
-                    re.accept(cv, 0);
-                }
+            return name.endsWith(".class") && Configuration.processPackage(name);
+        }).collect(Collectors.toList());
+
+        for (JarEntry entry: entries) {
+            try (InputStream stream = new BufferedInputStream(jarFile.getInputStream(entry), 1024)) {
+                ClassReader re = new ClassReader(stream);
+                ClassDescriptor cv = new ClassDescriptor(stream);
+                re.accept(cv, 0);
             }
         }
 
